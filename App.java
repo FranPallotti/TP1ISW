bf7ceccd278da49e0545747208f202f1c21ae4af
egarello <enzogarello5@gmail.com>
2024-09-24 12:19:48 -0300

    Modificar - Estado B

--- App.java
+++ App.java
@@ -1,11 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-
-        System.out.println("Adios Mundo");
-
-        System.out.println("Cambio en funcionalidad - h");
-        System.out.println("Modificación - Estado A");
+        
+        System.out.println("Modificación - Estado B.");
     }
 
 }
