d18caab1eba5ad4ebb7bbf1c5e309145e960a090
egarello <enzogarello5@gmail.com>
2024-09-24 12:25:54 -0300

    Revert "Modificar - Estado B"
    This reverts commit bf7ceccd278da49e0545747208f202f1c21ae4af.

--- App.java
+++ App.java
@@ -1,7 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        
-        System.out.println("Modificación - Estado B.");
+        System.out.println("Hola Mundo");
+
+        System.out.println("Adios Mundo");
+
+        System.out.println("Cambio en funcionalidad - h");
+        System.out.println("Modificación - Estado A");
     }
 
 }
